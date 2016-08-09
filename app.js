8374240b6e88f8f2dc31b946cce6921bff2d37f3
Patrick <pcrussel@ucsc.edu>
2016-08-09 10:53:54 -0700

    Improved Look
    Also added more floating stuff. Progress, but also learning!

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var app = angular.module('forum', []);
 
+var post = {postName: "", avatar: "", postBody: ""}
+
+var posts = [ {postName:"kek", avatar:"/FakePics/Pat.jpg", postBody:"Hey guys"}, {postName:"Foo", avatar:"/FakePics/Doggo1.jpg", postBody: "Doing good. U?"}]
+
 app.controller('ForumPost', function(){
-    this.postName = "Kek - God of Chaos";
-    this.postBody = "Check 'em ->"
-    
+    this.posts = posts;
 });
