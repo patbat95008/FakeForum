a687718027f7ad8e2cb6c0c13f6539136dc74a5c
Patrick <pcrussel@ucsc.edu>
2016-08-10 10:29:07 -0700

    Added a Reply Field
    It doesn't do anything yet, but hey, progress!

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var app = angular.module('forum', []);
 
-var post = {postName: "", avatar: "", postBody: ""}
-
 var posts = [ {postName:"Lord Kek", avatar:"F:/WebPages/FakeForum/FakeForum/FakePics/Pat.jpg", postBody:"Hey guys; what up?"}, 
     {postName:"Jei", avatar:"F:/WebPages/FakeForum/FakeForum/FakePics/Doggo1.jpg", postBody: "Doing good. U?"}];
 
+var title = "State of the Forum";
+
 app.controller('ForumPost', function(){
-    
+    this.title = title;
     this.posts = posts;
+});
+
+app.controller('ReplyForm', function(){
+    this.UserID="";
+    this.UserPost="";
+    
+    
+    this.reply = function (){
+        posts.push({postName: userID, avatar:"", postBody: userPost});
+        console.log('Clicked Post');
+    }
 });
