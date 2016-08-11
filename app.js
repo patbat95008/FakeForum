20910e04f4e0d637ff4851a3cc52c80ed1d611de
Patrick <pcrussel@ucsc.edu>
2016-08-11 15:43:18 -0700

    Happy with Last Push
    This is my final push. I learned a lot about both CSS, HTML5, and
    Angular.js. Hopefully I'll have more time to make this thing real at
    some other point in time. Until then, so long FF!

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var app = angular.module('forum', []);
 
 var posts = [ {postName:"Lord Kek", avatar:"F:/WebPages/FakeForum/FakeForum/FakePics/Pat.jpg", postBody:"Hey guys; what up?"}, 
-    {postName:"Jei", avatar:"F:/WebPages/FakeForum/FakeForum/FakePics/Doggo1.jpg", postBody: "Doing good. U?"}];
+    {postName:"Jei", avatar:"F:/WebPages/FakeForum/FakeForum/FakePics/Doggo1.jpg", postBody: "Doing good. U?"},
+    {postName:"Anon", avatar:"F:/WebPages/FakeForum/FakeForum/FakePics/Doggo2.jpg", postBody: "     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et pulvinar massa. Mauris ultrices nulla sed est aliquet molestie. Quisque ultricies neque non turpis hendrerit dapibus.\n\n    Pellentesque suscipit, est id varius venenatis, quam tellus maximus leo, tristique dictum justo leo vitae tortor. Phasellus auctor, odio nec feugiat interdum, tortor erat lobortis ligula, nec ultrices nisl quam at turpis. Duis a tempus risus, eu vulputate lectus. Donec vulputate purus a fermentum viverra. Suspendisse consequat, erat eget mollis dictum, libero lectus tempor lacus, non tristique ex nunc non lacus. Sed posuere lacus vel mi pellentesque eleifend. Nam ut nibh sed nisi molestie porttitor. Aliquam erat volutpat. Sed sed nunc commodo, luctus dolor in, elementum sem. Praesent rhoncus libero diam, vel lobortis elit tristique a. Fusce eget facilisis tortor. Pellentesque non tortor justo."},
+    {postName:"Jei", avatar:"F:/WebPages/FakeForum/FakeForum/FakePics/Doggo1.jpg", postBody: "LOL wut"}
+    ];
 
 var title = "State of the Forum";
 
